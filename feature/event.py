7f3b09c06aeaf5be00f49cae595130f3616f88aa
mobiiii <13988486+mobiiii@user.noreply.gitee.com>
2024-06-01 20:36:54 +0800

    111

--- feature/event.py
+++ feature/event.py
@@ -4,9 +4,7 @@
 
 import librosa
 import librosa.display
-import matplotlib.pyplot as plt
 import numpy as np
-import Ampitude_Envelope
 from scipy.signal import find_peaks
 
 event_freq = 2
@@ -14,6 +12,46 @@ height = 200
 window_len = 2048
 hop_len = 1024
 bits = 8
+
+class EnvelopeGenerate:
+    def __init__(self, window_len:int=2048, hop_len:int=1024, bits:int=8) -> None:
+        self.window_len = window_len
+        self.hop_len = hop_len
+        self.bits = bits
+
+    def process_env(self,waveform):
+        if (len(waveform)-self.window_len) % self.hop_len != 0:
+            frame_num = int((len(waveform)-self.window_len)/self.hop_len) + 1
+            pad_num = frame_num*self.hop_len + self.window_len - len(waveform)
+            waveform = np.pad(waveform,(0,pad_num),mode='edge')
+        frame_num = int((len(waveform)-self.window_len)/self.hop_len)
+        waveform_ae = []
+        for t in range(frame_num):
+            current_frame = waveform[t*self.hop_len:(t*self.hop_len + self.window_len)]
+            current_ae = max(current_frame)
+            waveform_ae.append(current_ae)
+        return np.array(waveform_ae)
+    
+    def normalize_env(self, envelope):
+        max_val = np.max(envelope)
+        min_val = np.min(envelope)
+        normalized_envelope = ((envelope - min_val) / (max_val - min_val)) * (2 ** self.bits - 1)
+        return normalized_envelope.astype(int)
+    
+    def save2file(self, envelope, filename):
+        if np.issubdtype(envelope.dtype, np.integer):
+            binary_strings = [format(int(val), '0{}b'.format(self.bits)) for val in envelope]
+            with open(filename, 'w') as file:
+                for a, b in zip(envelope, binary_strings):
+                    file.write(f"{a}\t{b}\n")
+        else:
+            with open(filename, 'w') as file:
+                np.savetxt(file, envelope)
+
+def output_env(window_len, hop_len, bits, waveform):
+    ae_generate = EnvelopeGenerate(window_len=window_len, hop_len=hop_len, bits=bits)
+    envelope = ae_generate.process_env(waveform)
+    return envelope
 
 def seg_env_gen(waveform, sr):
     #分段
@@ -24,7 +62,7 @@ def seg_env_gen(waveform, sr):
     envelopes = []
     magnitudes = []
     for segment in segments:
-        envelope = Ampitude_Envelope.output_env(window_len=window_len,hop_len=hop_len,bits=bits,waveform=segment)
+        envelope = output_env(window_len=window_len,hop_len=hop_len,bits=bits,waveform=segment)
 
         #归一化
         max_val = np.max(waveform)
