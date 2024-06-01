3f01b504ff3e652cd84fc1442f0fb7296efc71aa
mobiiii <13988486+mobiiii@user.noreply.gitee.com>
2024-06-01 19:54:14 +0800

    event generater

--- feature/event.py
+++ feature/event.py
@@ -9,14 +9,15 @@ import numpy as np
 import Ampitude_Envelope
 from scipy.signal import find_peaks
 
-segment_length = 8820
+event_freq = 2
 height = 200
 window_len = 2048
 hop_len = 1024
 bits = 8
 
-def seg_env_gen(waveform):
+def seg_env_gen(waveform, sr):
     #分段
+    segment_length = sr // event_freq
     num_segments = len(waveform) // segment_length
     segments = np.array_split(waveform[:num_segments * segment_length], num_segments)
 
@@ -29,12 +30,13 @@ def seg_env_gen(waveform):
         max_val = np.max(waveform)
         min_val = 0
         nor_env = ((envelope - min_val) / (max_val - min_val)) * (2 ** bits - 1)
+        nor_env = nor_env.astype(int)
         envelopes.append(nor_env)
 
         #包络详细信息
         max = np.max(nor_env)
         min = np.min(nor_env)
-        mean = np.mean(nor_env)
+        mean = int(np.mean(nor_env))
         diff = max - min
         magnitude = [max, min, mean, diff]
         magnitudes.append(magnitude)
@@ -43,10 +45,12 @@ def seg_env_gen(waveform):
 
 def seg_freq_gen(waveform, sr):
     #分段
+    segment_length = sr // event_freq
     num_segments = len(waveform) // segment_length
     segments = np.array_split(waveform[:num_segments * segment_length], num_segments)
 
     seg_avg_freqs = []
+    main_freqs = []
     for i, segment in enumerate(segments):
 
         # 执行FFT
@@ -60,49 +64,195 @@ def seg_freq_gen(waveform, sr):
 
         peaks, _ = find_peaks(np.abs(fft_result)[:len(segment)//2], height=height,distance=min_peak_distance)
 
+        
         if len(peaks) == 0:
             peak_frequencies = np.array([0])
             peak_amplitudes = np.array([0])
             weighted_avg_frequency = 0
+            main_freq = 0
         else:
             peak_frequencies = frequencies[peaks]
             peak_amplitudes = np.abs(fft_result[peaks])
             weighted_avg_frequency = np.sum(peak_frequencies * (peak_amplitudes - height)) / np.sum(peak_amplitudes - height)
-
+            weighted_avg_frequency = weighted_avg_frequency.astype(int)
+            max_amplitude_idx = np.argmax(peak_amplitudes)
+            main_freq = peak_frequencies[max_amplitude_idx]
+            
         seg_avg_freqs.append(weighted_avg_frequency)
+        main_freqs.append(main_freq)
 
-    return seg_avg_freqs
+    #归一化
+    max_val = np.max(seg_avg_freqs)
+    min_val = np.min(seg_avg_freqs)
+    nor_seg_avg_freqs = ((seg_avg_freqs - min_val) / (max_val - min_val)) * (2 ** bits - 1)
+    nor_seg_avg_freqs = nor_seg_avg_freqs.astype(int)
 
-def event_args_gen(magnitude, freq):
-    out_freq = 0
-    duty_cycle = 0
-    amp = '00'
+    return seg_avg_freqs, main_freqs
 
-    #幅值与占空比
+def event_gen(magnitude, freq):
+    event = 0
 
-    #频率
-    if freq == 0:
-        duty_cycle = 5
+    if(magnitude[0] < 5 or freq < 20):
+        event = 0
+    elif(freq >= 20 and freq < 100):
+        if((magnitude[0] / magnitude[2]) > 2 and magnitude[2] < 36): #类似kick，只敲一下，其他时刻幅度接近0
+            if(magnitude[0] < 36):
+                event = 1
+            elif(magnitude[0] < 72):
+                event = 2
+            elif(magnitude[0] < 109):
+                event = 3
+            elif(magnitude[0] < 145):
+                event = 4
+            elif(magnitude[0] < 181):
+                event = 5
+            elif(magnitude[0] < 217):
+                event = 6
+            elif(magnitude[0] <= 255):
+                event = 7
+            else:
+                event = 0
+        else: #其他普通低频情况
+            if(magnitude[2] < 36):
+                event = 1
+            elif(magnitude[2] < 72):
+                event = 2
+            elif(magnitude[2] < 109):
+                event = 3
+            elif(magnitude[2] < 145):
+                event = 4
+            elif(magnitude[2] < 181):
+                event = 5
+            elif(magnitude[2] < 217):
+                event = 6
+            elif(magnitude[2] <= 255):
+                event = 7
+            else:
+                event = 0
+    elif(freq >= 100 and freq < 180):
+        if((magnitude[0] / magnitude[2]) > 2 and magnitude[2] < 36): #类似kick，只敲一下，其他时刻幅度接近0
+            if(magnitude[0] < 42):
+                event = 8
+            elif(magnitude[0] < 85):
+                event = 9
+            elif(magnitude[0] < 127):
+                event = 10
+            elif(magnitude[0] < 170):
+                event = 11
+            elif(magnitude[0] < 212):
+                event = 12
+            elif(magnitude[0] <= 255):
+                event = 13
+            else:
+                event = 0
+        else: #其他普通情况
+            if(magnitude[2] < 42):
+                event = 8
+            elif(magnitude[2] < 85):
+                event = 9
+            elif(magnitude[2] < 127):
+                event = 10
+            elif(magnitude[2] < 170):
+                event = 11
+            elif(magnitude[2] < 212):
+                event = 12
+            elif(magnitude[2] <= 255):
+                event = 13
+            else:
+                event = 0
+    elif(freq >= 180 and freq < 300): 
+        if((magnitude[0] / magnitude[2]) > 2 and magnitude[2] < 36): #类似kick，只敲一下，其他时刻幅度接近0
+            if(magnitude[0] < 51):
+                event = 14
+            elif(magnitude[0] < 102):
+                event = 15
+            elif(magnitude[0] < 153):
+                event = 16
+            elif(magnitude[0] < 204):
+                event = 17
+            elif(magnitude[0] <= 255):
+                event = 18
+            else:
+                event = 0
+        else: #其他普通情况
+            if(magnitude[2] < 51):
+                event = 14
+            elif(magnitude[2] < 102):
+                event = 15
+            elif(magnitude[2] < 153):
+                event = 16
+            elif(magnitude[2] < 204):
+                event = 17
+            elif(magnitude[2] <= 255):
+                event = 18
+            else:
+                event = 0
+    elif(freq >= 300 and freq < 500):
+        if((magnitude[0] / magnitude[2]) > 2 and magnitude[2] < 36): #类似kick，只敲一下，其他时刻幅度接近0
+            if(magnitude[0] < 85):
+                event = 19
+            elif(magnitude[0] < 170):
+                event = 20
+            elif(magnitude[0] <= 255):
+                event = 21
+            else:
+                event = 0
+        else: #其他普通情况
+            if(magnitude[2] < 85):
+                event = 19
+            elif(magnitude[2] < 170):
+                event = 20
+            elif(magnitude[2] <= 255):
+                event = 21
+            else:
+                event = 0
+    elif(freq >= 500 ):
+        if((magnitude[0] / magnitude[2]) > 2 and magnitude[2] < 36): #类似kick，只敲一下，其他时刻幅度接近0
+            if(magnitude[0] < 63):
+                event = 22
+            elif(magnitude[0] < 128):
+                event = 23
+            elif(magnitude[0] < 191):
+                event = 24
+            elif(magnitude[0] <= 255):
+                event = 25
+            else:
+                event = 0
+        else: #其他普通情况
+            if(magnitude[2] < 63):
+                event = 22
+            elif(magnitude[2] < 128):
+                event = 23
+            elif(magnitude[2] < 191):
+                event = 24
+            elif(magnitude[2] <= 255):
+                event = 25
+            else:
+                event = 0
+
+    return event
+
+def run(waveform, sr, output_path):
+    envelopes, magnitudes, num_segs = seg_env_gen(waveform, sr)
+    avg_freqs, main_freqs = seg_freq_gen(waveform, sr)
+
+    event = 0
+
+    with open(output_path, 'w') as file:
+
+        for i in range(num_segs):
+            event = event_gen(magnitudes[i], main_freqs[i])
+            file.write(f"{event}\n")
 
 
-    else:
-        out_freq = (freq / 1000) * 50
-
-
-    return out_freq, duty_cycle, amp
-
-def run(waveform, sr):
-    envelopes, magnitudes, num_segs = seg_env_gen(waveform)
-    freqs = seg_freq_gen(waveform, sr)
-
-    args_tbl = []
-    for i in num_segs:
-        freq, duty_cycle, amp = event_args_gen(magnitudes[i], freqs[i])
-        args = [freq, duty_cycle, amp]
-        args_tbl.append(args)
-
-    return args_tbl
-
+if __name__ == "__main__" :
+    filename = r'D:\ROP-Audio-Vibration\audio\Liangzhu.wav'
+    waveform, sr = librosa.load(filename, sr=None) 
+    output_path = r'D:\ROP-Audio-Vibration\test\output.txt'
+    print(event_freq)
+    print(sr)
+    run(waveform, sr, output_path)
+   
 
 
 
---
+++ feature/test_event.py
@@ -0,0 +1,105 @@
+#思路：按照测试结果，取event变化频率为5Hz，即event片段长度为200ms，即在44.1KHz采样得到的音频信号中，每8820个样本为一个event
+#考虑到event之间转换的平滑处理，在event间添加2205或2940个样本的过渡带，即20Hz或15Hz，测试得到人能分辨的刺激频率在0-20Hz左右
+#在每段event中，提取max,min,rms,频率等作为参数
+
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+import Ampitude_Envelope
+from scipy.signal import find_peaks
+
+filename = r'D:\ROP-Audio-Vibration\audio\kick.wav'
+waveform, sr = librosa.load(filename, sr=None) 
+output_path = r'D:\ROP-Audio-Vibration\test\output.txt'
+
+#对音频信号分段，每段对应一个event
+event_freq = 2
+segment_length = sr // event_freq
+num_segments = len(waveform) // segment_length
+segments = np.array_split(waveform[:num_segments * segment_length], num_segments)
+
+# #打印每段长度
+# for i, segment in enumerate(segments):
+#     print("Segment", i+1, "length:", len(segment))
+# print(segments[25])
+
+with open(output_path, 'w') as file:
+
+
+    #提取每一段的包络
+    envelopes = []
+    magnitudes = []
+    for i, segment in enumerate(segments):
+        envelope = Ampitude_Envelope.output_env(window_len=2048,hop_len=1024,bits=8,waveform=segment)
+        #归一化
+        max_val = np.max(waveform)
+        min_val = 0
+        nor_env = ((envelope - min_val) / (max_val - min_val)) * (2 ** 8 - 1)
+        nor_env = nor_env.astype(int)
+        envelopes.append(nor_env)
+        #包络详细信息
+        max = np.max(nor_env)
+        min = np.min(nor_env)
+        mean = int(np.mean(nor_env))
+        diff = max - min
+        magnitude = [max, min, mean, diff]
+        magnitudes.append(magnitude)
+        
+
+
+    #提取每一段的频率信息
+    #for i, segment in enumerate(segments):
+        # 执行FFT
+        fft_result = np.fft.fft(segment)
+        # 计算频率
+        frequencies = np.fft.fftfreq(len(segment), 1/sr)
+        min_freq_distance_hz = 150
+        min_peak_distance = int(min_freq_distance_hz * len(segment) / sr)
+        peaks, _ = find_peaks(np.abs(fft_result)[:len(segment)//2], height=200,distance=min_peak_distance)
+
+       
+
+            # 提取大于200的峰值对应的频率
+        if len(peaks) == 0:
+            peak_frequencies = np.array([0])
+            peak_amplitudes = np.array([0])
+            weighted_avg_frequency = 0
+            main_freq = 0
+        else:
+            peak_frequencies = frequencies[peaks]
+            peak_amplitudes = np.abs(fft_result[peaks])
+            weighted_avg_frequency = np.sum(peak_frequencies * (peak_amplitudes - 200)) / np.sum(peak_amplitudes - 200)
+            weighted_avg_frequency = weighted_avg_frequency.astype(int)
+            max_amplitude_idx = np.argmax(peak_amplitudes)
+            main_freq = peak_frequencies[max_amplitude_idx]
+
+        file.write(f"{i}: {magnitude}\t{peak_frequencies}\t{peak_amplitudes}\t{main_freq}\n")
+        # print(i)
+        # print(peak_frequencies)
+        # print(peak_amplitudes)
+        # print(weighted_avg_frequency)
+
+
+ # 执行FFT
+fft_result = np.fft.fft(segments[14])
+# 计算频率
+frequencies = np.fft.fftfreq(len(segments[14]), 1/sr)
+min_freq_distance_hz = 150
+min_peak_distance = int(min_freq_distance_hz * len(segments[14]) / sr)
+peaks, _ = find_peaks(np.abs(fft_result)[:len(segments[14])//2], height=200,distance=min_peak_distance)
+# 绘制振幅谱图
+plt.plot(segments[14], label='Waveform')
+plt.title('Waveform')
+plt.xlabel('Samples')
+plt.ylabel('Amplitude')
+plt.legend()
+
+plt.figure(figsize=(8, 4))
+#plt.plot(np.arange(22050), np.abs(fft_result))
+plt.plot(frequencies[:len(segments[14])//2], np.abs(fft_result)[:len(segments[14])//2])
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Amplitude')
+plt.title('FFT Result')
+plt.grid()
+plt.show()
